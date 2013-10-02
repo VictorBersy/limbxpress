c19dd6ffc5488aee529f59486b243936fc47c132
Victor Bersy <bigvivi.fr@gmail.com>
2013-10-03 01:20:42 +0200

    Admin config init

--- src/LimbXpress/Admin/Config/Config.php
+++ src/LimbXpress/Admin/Config/Config.php
@@ -22,13 +22,14 @@ class Config
 
         /* Translation Service Config */
         $app->register(new \Silex\Provider\TranslationServiceProvider(), array(
+          'locale'          => 'en_US',
           'locale_fallback' => 'en_US',
         ));
         /* Translation Service Provider */
         $app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
           $translator->addLoader('yaml', new YamlFileLoader());
-          $translator->addResource('yaml', __DIR__.'/../locales/en_US.yml', 'en_US');
-          $translator->addResource('yaml', __DIR__.'/../locales/fr_FR.yml', 'fr_FR');
+          $translator->addResource('yaml', __DIR__.'/../Locales/en_US.yml', 'en_US');
+          $translator->addResource('yaml', __DIR__.'/../Locales/fr_FR.yml', 'fr_FR');
           return $translator;
         }));
     }
